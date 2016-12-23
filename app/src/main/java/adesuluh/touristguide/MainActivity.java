5a810675e841f83471a971ae2529bad2a5f26449
Ade Suluh <ade.suluh@gmail.com>
2016-12-24 00:24:36 +0700

    Add Little New Look

--- app/src/main/java/adesuluh/touristguide/MainActivity.java
+++ app/src/main/java/adesuluh/touristguide/MainActivity.java
@@ -3,6 +3,8 @@ package adesuluh.touristguide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -50,5 +52,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.items, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
